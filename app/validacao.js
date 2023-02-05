6b10ade50c3525bedf1f301b4fe5ae1d78e98330
ThiagoP15 <thiporto84@gmail.com>
2023-02-04 23:11:16 -0300

    game over

--- app/validacao.js
+++ app/validacao.js
@@ -10,6 +10,22 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
         return
     }
+    if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
+    }
+
 
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
